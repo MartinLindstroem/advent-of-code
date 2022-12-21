34846483bb9a55cf0e4ad65d47aa78fb788443e8
Martin Lindström <martinllindstroem@gmail.com>
2022-12-21 11:07:28 +0100

    day 6

---
+++ 2022/day6/1.py
@@ -0,0 +1,22 @@
+def isDuplicate(chars):
+    for char in chars:
+        if chars.count(char) > 1:
+            return True
+    return False
+
+def signal_marker():
+    file = open("input.txt", "r")
+    data = file.readlines()
+
+    start_index = 0
+
+    for char in data[0]:
+        end_index = start_index + 4
+        char_set = data[0][start_index:end_index]
+
+        if not isDuplicate(char_set):
+            return (end_index)
+
+        start_index += 1
+
+print(signal_marker())

f4dc20641b6ae1c65ced06312f71316e855c3998
Martin <martinllindstroem@gmail.com>
2023-12-11 21:19:38 +0100

    update

--- 2023/day1/2.py
+++ 2023/day1/2.py
@@ -1,32 +1,26 @@
+import re
+
 file = open("input.txt", "r")
+# file = open("test_input.txt", "r")
 lines = file.readlines()
 spelled_out_numbers = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
-calibration_value = None
+pattern = r'(?:one|two|three|four|five|six|seven|eight|nine|\d)'
 total = 0
-numbers = []
 
-
-for line in lines[5:6]:
+for line in lines:
     line = line.strip()
-
-    # for char in line:
-    #     if char.isdigit():
-    #         numbers.append(char)
-    # calibration_value = numbers[0] + numbers[-1]
-    # total += int(calibration_value)
-
-    for nr in spelled_out_numbers:
-        if nr in line:
-            print(line.count(nr), nr)
-            numbers.append(spelled_out_numbers[nr])
-        if spelled_out_numbers[nr] in line:
-            print(line.count(spelled_out_numbers[nr]), spelled_out_numbers[nr])
-            numbers.append(spelled_out_numbers[nr])
+    numbers = re.findall(pattern, line)
+    print(line)
     
+    for number in numbers:
+        for alpha_num in spelled_out_numbers:
+            if number == alpha_num:
+                idx = numbers.index(number)
+                numbers[idx] = spelled_out_numbers[number]
+    
+    calibration_value = numbers[0] + numbers[-1]
+    # print(calibration_value)
+    total += int(calibration_value)
 
-
-    # print(numbers)
-    # numbers = []
-
-# print(total)
+print(total)
